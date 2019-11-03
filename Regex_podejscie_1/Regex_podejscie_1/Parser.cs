639b936f726d50372592d393c186bb9a6fdf6455
Bartek <bartekbednarek51@gmail.com>
2019-11-03 15:26:13 +0100

    inicjalizacja projektu

---
+++ Regex_podejscie_1/Regex_podejscie_1/Parser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Parser
+{
+    public string path;
+
+    public Parser(string path)
+    {
+        this.path = path;
+    }
+
+    public void fileOpen()
+    {
+        string text = System.IO.File.ReadAllText(this.path);
+        
+        //----test------
+        Console.WriteLine(text);
+        //--------------
+    }
+}
---
+++ Regex_podejscie_1/Regex_podejscie_1/Wezel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Wezel
+{
+    public int OID;
+    public string Value;
+
+    public Wezel(int iod, string value)
+    {
+        this.OID = iod;
+        this.Value = value;
+    }
+}
