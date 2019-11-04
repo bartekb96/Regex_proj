ff2a5904d8be2969572c4a4a3f01337bd5c05372
Bartek <bartekbednarek51@gmail.com>
2019-11-04 22:10:45 +0100

    oczyt z pliku i przeniesienie kodu Parsera

--- Regex_podejscie_1/Regex_podejscie_1/Parser.cs
+++ Regex_podejscie_1/Regex_podejscie_1/Parser.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public class Parser
 {
     public string path;
+
+    private string content; 
 
     public Parser(string path)
     {
@@ -11,10 +16,48 @@ public class Parser
 
     public void fileOpen()
     {
-        string text = System.IO.File.ReadAllText(this.path);
-        
-        //----test------
-        Console.WriteLine(text);
-        //--------------
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(this.path))
+            {
+                content = sr.ReadToEnd();
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+
     }
+
+    public List<Wezel> pharse()
+    {
+        List<Wezel> wezly = new List<Wezel>();
+
+        int OID;
+        string value;
+        List<string> obiekty = new List<string>();
+
+        string OID_pattern = @"OID: [0-9]";
+        string Value_pattern = @"[ ]WARTOSC: [^ ]+";
+        string Object_pattern = @"OID: [0-9] +WARTOSC: +[^ ]+";
+
+        foreach (Match match in Regex.Matches(content, Object_pattern, RegexOptions.IgnoreCase))
+        {
+            obiekty.Add(match.Value);
+        }
+
+        foreach (string ob in obiekty)
+        {
+            Int32.TryParse(Regex.Replace(Regex.Replace(ob, Value_pattern, String.Empty), "OID: ", String.Empty), out OID);
+            value = (Regex.Replace(Regex.Replace(ob, OID_pattern, String.Empty), "WARTOSC: ", String.Empty));
+            wezly.Add(new Wezel(OID, value));
+        }
+
+        return wezly;
+    }
+
+
 }
--- Regex_podejscie_1/Regex_podejscie_1/Program.cs
+++ Regex_podejscie_1/Regex_podejscie_1/Program.cs
@@ -8,40 +8,20 @@ namespace Regex_podejscie_1
     {
         static void Main(string[] args)
         {
-            int OID = 0;
-            string value = "1111";
-            List<string> obiekty = new List<string>();
             List<Wezel> wezly = new List<Wezel>();
 
-            string OID_pattern = @"OID: [0-9]";
-            string Value_pattern = @"[ ]WARTOSC: [^ ]+";
-            string Object_pattern = @"OID: [0-9] +WARTOSC: +[^ ]+";
-
-            string input = "OID: 1 WARTOSC: kot OID: 2 WARTOSC: pies OID: 3 WARTOSC: ryba";
-
-            foreach (Match match in Regex.Matches(input, Object_pattern, RegexOptions.IgnoreCase))
-            {
-                obiekty.Add(match.Value);
-            }
-
-            foreach (string ob in obiekty)
-            {
-                Int32.TryParse(Regex.Replace(Regex.Replace(ob, Value_pattern, String.Empty), "OID: ", String.Empty), out OID);
-                value = (Regex.Replace(Regex.Replace(ob, OID_pattern, String.Empty), "WARTOSC: ", String.Empty));
-                wezly.Add(new Wezel(OID, value));
-            }
+            Parser parser = new Parser(@"C:\Users\Bartek\source\repos\Regex_proj\Regex_podejscie_1\Regex_podejscie_1\test.txt");
+            parser.fileOpen();
+            wezly = parser.pharse();
 
             //---------------SPRAWDZENIE---------------------------
 
-            /*foreach (Wezel w in wezly)
+            foreach (Wezel w in wezly)
             {
                 Console.WriteLine("oid wynosi: " + w.OID + ", wartosc wynosi: " + w.Value );
-            }*/
+            }
 
             //-----------------------------------------------------
-
-            Parser parser = new Parser(@"C:\Users\Bartek\source\repos\Regex_podejscie_1\Regex_podejscie_1\test.txt");
-            parser.fileOpen();
         }
     }
 }
